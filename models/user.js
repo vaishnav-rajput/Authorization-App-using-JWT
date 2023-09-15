1e5d7eb8caeb8c3c71a2345f3c996491e90f5364
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-09-15 19:47:06 +0530

    route created, user model created

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["Admin", "Student", "Visitor"] //using enum means it will only accept the value within these three values
+    }
+})
