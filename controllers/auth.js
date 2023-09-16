8d302d6271fb5c478b743a92d68abbe6c967322d
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-09-16 17:14:43 +0530

    cookie and options created

--- controllers/auth.js
+++ controllers/auth.js
@@ -83,7 +83,21 @@ exports.login = async(req, res) => {
         if(await bcrypt.compare(password, user.password)){
             //password match
             let token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "2h",})
+            user.token = token;
+            user.password = undefined
 
+
+            const options = {
+                expires: new Date( Date.now() + 3*24*60*60*1000),
+                httpOnly: true,
+
+            }
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                token,
+                user,
+                message: "User logged in successfully"
+            })
   
         }else{
             //password do not match
@@ -95,6 +109,10 @@ exports.login = async(req, res) => {
 
 
     } catch (error) {
-        
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            message: "Login failure",
+        })
     }
 }
