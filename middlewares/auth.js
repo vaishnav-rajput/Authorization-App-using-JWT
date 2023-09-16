f76bb83472760f7248c10bc80f2ecbd0f35ca029
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-09-16 17:55:02 +0530

    protected routes

---
+++ middlewares/auth.js
@@ -0,0 +1,1 @@
+//auth , isStudent, isAdmin
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,18 @@ const express = require("express")
 const router = express.Router()
 
 const {login, signup} = require("../controllers/auth")
+const {auth, isAdmin, isStudent} = require("../middlewares/auth")
 
 router.post("/login", login)
 router.post("/signup", signup)
+
+
+//protected route
+router.get("/student", auth, isStudent, (req,res) => {
+    res.json({
+        success: true,
+        message:"Welcome"
+    })
+})
 
 module.exports = router
