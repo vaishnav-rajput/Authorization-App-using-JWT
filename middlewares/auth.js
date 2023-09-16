b1d8be1ba3533d8afdb3b43d742272313a5a6273
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-09-16 18:32:19 +0530

    isAdmin middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -46,6 +46,26 @@ exports.isStudent = (req,res,next) => {
         }
         next()
     } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "user role is not matching"
+        })
+    }
+}
 
+exports.isAdmin = (req, res, next)  => {
+    try{
+        if(req.user.role !== "Admin"){
+            return res.status(401).json({
+                success: "false",
+                message: "this is a protected route for Admin "
+            })
+        }
+        next()
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "user role is not matching"
+        })
     }
 }
